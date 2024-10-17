ec19fadee74ffe5df6b5fe4f1238f7449b8f0bf2
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-10-17 17:06:02 +0100

    convert route.ts to route.js

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,16 +1,13 @@
 import NextAuth from "next-auth";
 import authOptions from "../../../auth";
 
-// Create a single handler for all HTTP methods
-export default async function handler(req, res) {
-  if (req.method === "GET") {
-    console.log("GET request received at /api/auth/[...nextauth]");
-    return NextAuth(authOptions)(req, res);
-  } else if (req.method === "POST") {
-    console.log("POST request received at /api/auth/[...nextauth]");
-    return NextAuth(authOptions)(req, res);
-  } else {
-    res.setHeader("Allow", ["GET", "POST"]);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
-}
+// Export the NextAuth handlers
+export const GET = async (req, res) => {
+	console.log("GET request received at /api/auth/[...nextauth]");
+	return NextAuth(authOptions)(req, res);
+};
+
+export const POST = async (req, res) => {
+	console.log("POST request received at /api/auth/[...nextauth]");
+	return NextAuth(authOptions)(req, res);
+};
