49b575ffa4ff99f9ee287ac467e2817e0bcfd70e
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-10-17 05:19:03 +0100

    improve on the overall ui

--- src/app/chat/page.js
+++ src/app/chat/page.js
@@ -3,7 +3,7 @@ import Chatbot from "../components/Chatbot";
 export default function Chat() {
 	return (
 		<div className="min-h-screen bg-gray-50 flex flex-col items-center">
-			<h1 className="text-3xl font-bold text-indigo-600 mt-6">
+			<h1 className="text-3xl font-bold text-indigo-600 mt-6 mx-auto">
 				Welcome to SmartFin Chatbot
 			</h1>
 			<Chatbot />
