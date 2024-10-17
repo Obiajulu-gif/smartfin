410e6a6465b073ae8fca4837dc2b416b292f8ae4
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-10-17 16:41:39 +0100

    fixing error

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,13 +1,16 @@
 import NextAuth from "next-auth";
 import authOptions from "../../../auth";
 
-// Export the NextAuth handlers
-export const GET = async (req, res) => {
+// Create a single handler for all HTTP methods
+export default async function handler(req, res) {
+  if (req.method === "GET") {
     console.log("GET request received at /api/auth/[...nextauth]");
     return NextAuth(authOptions)(req, res);
-};
-
-export const POST = async (req, res) => {
+  } else if (req.method === "POST") {
     console.log("POST request received at /api/auth/[...nextauth]");
     return NextAuth(authOptions)(req, res);
-};
+  } else {
+    res.setHeader("Allow", ["GET", "POST"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
